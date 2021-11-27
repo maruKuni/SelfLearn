90a6e333fcfaeb74f5343dea9c07344175365944
國丸裕太 <yu.kuni.maru@gmail.com>
2021-11-28 08:55:54 +0900

    report準備

--- thread/compareMatrix.sh
+++ thread/compareMatrix.sh
@@ -5,10 +5,12 @@ fi
 if [ -f "resultMatrixThread.data" ]; then
     rm resultMatrixThread.data
 fi
-SIZE=5
-while [ ${SIZE} -le 5000 ] 
+SIZE=3
+while [ ${SIZE} -le 3000 ] 
 do
     echo -n "${SIZE} " >> resultMatrixSimple.data
+    echo -n "${SIZE} " >> resultMatrixThread.data
     (\time -f "%U %S %e" ./simpleMatrix ${SIZE}) 2>> resultMatrixSimple.data
+    (\time -f "%U %S %e" ./T_Matrix_proved ${SIZE}) 2>> resultMatrixThread.data
     SIZE=$((SIZE * 10))
 done
